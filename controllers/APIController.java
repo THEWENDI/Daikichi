037ed1fe8c00f4aa666f61fca6b8f27d1961207c
Wendi Li <issacbbb@hotmaill.com>
2022-02-10 18:05:13 -0800

    initial commit

---
+++ controllers/APIController.java
@@ -0,0 +1,31 @@
+package com.wendi.Daikichi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class APIController {
+	
+	@GetMapping("")
+	public String index() {
+		return "Welcome!";
+	}
+	
+	@GetMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@GetMapping("/tomorrow")
+	public String tmr() {
+		return "Tomorrow, an opportunity will arise,so be sure to be open to new ideas!";
+	}
+	
+	
+	
+	
+	
+	
+}
