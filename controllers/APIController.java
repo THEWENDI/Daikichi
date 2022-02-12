a9127f084c5d048a161979c01523816286abbada
Wendi Li <issacbbb@hotmaill.com>
2022-02-11 20:00:21 -0800

    with the new path

--- controllers/APIController.java
+++ controllers/APIController.java
@@ -1,6 +1,7 @@
 package com.wendi.Daikichi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +24,19 @@ public class APIController {
 		return "Tomorrow, an opportunity will arise,so be sure to be open to new ideas!";
 	}
 	
+	@GetMapping("/travel/{place}")
+	public String travel(@PathVariable("place")String place) {
+		return "Congratulations! You will soon travel to "+ place + "!";
+	}
 	
-	
+	@GetMapping("/lotto/{lottonum}")
+	public String travel(@PathVariable("lottonum")Integer lottonum) {
+		if(lottonum % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";		
+		}else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+	}
 	
 	
 	
